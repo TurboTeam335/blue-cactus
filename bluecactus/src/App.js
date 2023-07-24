1cd5e0bd7ad21c8015b053a50f39776b52f686f1
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-07-24 12:21:37 -0500

    git init

---
+++ bluecactus/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import PortfolioContainer from "./components/PortfolioContainer/PortfolioContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <PortfolioContainer />
+    </div>
+  );
+} 
+
+export default App;
---
+++ bluecactus/src/components/PortfolioContainer/PortfolioContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header/Header";
+import About from "../../containers/About/About";
+
+export default function PortfolioContainer() {
+  return (
+    <div>
+      <Header />
+      <About />
+    </div>
+  )
+}
