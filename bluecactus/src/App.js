b1b193e7d87d6e37e0cd0a5fdb2a1fe0092c8539
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-10-31 11:59:34 -0700

    scroll to top

--- bluecactus/src/App.js
+++ bluecactus/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Element } from 'react-scroll';
 import Header from './components/Header/Header';
 import HeroSection from './components/HeroSection/HeroSection';
@@ -8,17 +8,20 @@ import Contact from './containers/Contact/Contact';
 import Follow from './containers/Follow/Follow';
 import Footer from './components/Footer/Footer';
 
-
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="App">
       <Element name="top" />
       <Header />
       <HeroSection />
-      <Element name="about"><div ><About /></div></Element>
-      <Element name="gallery"><div ><Gallery /></div></Element>
+      <Element name="about"><About /></Element>
+      <Element name="gallery"><Gallery /></Element>
       <Element name="contact"><Contact /></Element>
-      <Element name="follow"><div><Follow /></div></Element>
+      <Element name="follow"><Follow /></Element>
       <Footer />
     </div>
   );
